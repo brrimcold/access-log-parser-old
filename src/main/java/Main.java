1d43365497a0e54c3d1990edc549e3651178fcd7
Kirill Petrov <brrimcold@yandex.ru>
2024-02-25 09:00:38 +0300

    added pair number arithmetic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int diff = firstNumber - secondNumber;
+        int mult = firstNumber * secondNumber;
+        double quotient = (double)firstNumber / secondNumber;
+        System.out.println("Сумма чисел равна: " + sum);
+        System.out.println("Разность чисел равна: " + diff);
+        System.out.println("Произведение чисел равна: " + mult);
+        System.out.println("Частное чисел равно: " + quotient);
+        }
     }
-}
